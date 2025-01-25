8ae10402dcba45460a54286ca62029bda473884c
Igor Chernyshov <ichernyshov@bk.ru>
2025-01-25 15:17:25 +0300

    (Kata) Remove Element

--- LeetCode.playground/Pages/Remove Element.xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/Remove Element.xcplaygroundpage/Contents.swift
@@ -1,17 +1,22 @@
 import Foundation
 
 func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
-	guard !nums.isEmpty else { return 0 }
-	var i = 0
+	var slow = 0
+	var fast = 0
 
-	for num in nums {
-		guard num != val else { continue }
-		nums[i] = num
-		i += 1
+	while fast < nums.count {
+		if nums[fast] != val {
+			nums.swapAt(slow, fast)
+			slow += 1
+		}
+		fast += 1
 	}
 
-	return i
+	return slow
 }
 
 var input = [3, 2, 2, 3]
-removeElement(&input, 3)
+removeElement(&input, 3) // 2
+
+var input2 = [0, 1, 2, 2, 3, 0, 4, 2]
+removeElement(&input2, 2) // 5
