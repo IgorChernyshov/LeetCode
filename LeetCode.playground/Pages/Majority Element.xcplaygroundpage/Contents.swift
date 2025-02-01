296939dcce3c439c86b4ce5cff49357128c598d3
Igor Chernyshov <ichernyshov@bk.ru>
2025-02-01 14:23:15 +0300

    (Kata) Majority Element

--- LeetCode.playground/Pages/Majority Element.xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/Majority Element.xcplaygroundpage/Contents.swift
@@ -5,5 +5,19 @@ func majorityElement(_ nums: [Int]) -> Int {
 	return sortedNums[middle]
 }
 
-majorityElement([3,2,3]) // 3
-majorityElement([2,2,1,1,1,2,2]) // 2
+func majorityElement2(_ nums: [Int]) -> Int {
+	var candidate = 0
+	var votes = 0
+
+	for num in nums {
+		if votes == 0 {
+			candidate = num
+		}
+		votes += (candidate == num) ? 1 : -1
+	}
+
+	return candidate
+}
+
+majorityElement2([3,2,3]) // 3
+majorityElement2([2,2,1,1,1,2,2]) // 2
