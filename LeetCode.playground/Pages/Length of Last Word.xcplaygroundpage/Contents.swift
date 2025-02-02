050e43e2ce1caca66dde7eed405890e1d7b48e70
Igor Chernyshov <ichernyshov@bk.ru>
2025-02-02 14:16:08 +0300

    (Kata) Length of the Last Word

--- LeetCode.playground/Pages/Length of Last Word.xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/Length of Last Word.xcplaygroundpage/Contents.swift
@@ -23,3 +23,19 @@ func lengthOfLastWordSplits(_ s: String) -> Int {
 lengthOfLastWordSplits("Hello World") // 5
 lengthOfLastWordSplits("   fly me   to   the moon  ") // 4
 lengthOfLastWordSplits("luffy is still joyboy") // 6
+
+func lengthOfLastWord2(_ s: String) -> Int {
+	var lengthOfLastWord = 0
+	for character in s.reversed() {
+		if character != " " {
+			lengthOfLastWord += 1
+		} else if lengthOfLastWord > 0 {
+			return lengthOfLastWord
+		}
+	}
+	return lengthOfLastWord
+}
+
+lengthOfLastWord2("Hello World") // 5
+lengthOfLastWord2("   fly me   to   the moon  ") // 4
+lengthOfLastWord2("luffy is still joyboy") // 6
